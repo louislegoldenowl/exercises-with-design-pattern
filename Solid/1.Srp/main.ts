1262531a0c705edd598058378a9d966b9d16c226
Louis <louis.le.goldenowl@gmail.com>
2026-08-17 10:54:30 +0700

    refactor(SRP): refactor code base on SRP

--- Solid/1.Srp/main.ts
+++ Solid/1.Srp/main.ts
@@ -1,69 +1,19 @@
-/**
- * *****************************************
- * 📝 UNCOMMENT THE CODE BELOW AND BEGIN YOUR SOLUTION:
- * *****************************************
- *
- * The following lines are currently commented out.
- * Uncomment them to start implementing your solution.
- * Happy coding! 🚀
- */
+export { Book } from './Book';
+export { Library } from './Library';
+export { BookSearch } from './BookSearch';
 
-// class Book {
-//     title: string;
-//     author: string;
-//     publicationYear: number;
-//     constructor(title: string, author: string, publicationYear: number) {
-//         this.title = title;
-//         this.author = author;
-//         this.publicationYear = publicationYear;
-//     }
-// }
+import { Book } from './Book';
+import { Library } from './Library';
 
-// class Library {
-//     books: Book[];
+let lib: Library;
+lib = new Library();
 
-//     constructor() {
-//         this.books = [];
-//     }
-//     addBook(book: Book): void {
-//         this.books.push(book);
-//     }
+let book1: Book;
+book1 = new Book('Clean Code', 'Edric Cao', 2023);
 
-//     getListBooks(): Book[] {
-//         return this.books;
-//     }
+let book2: Book;
+book2 = new Book('Design Pattern', 'Edric Cao', 2022);
 
-//     removeBook(title: string): void {
-//         this.books = this.books.filter(book => book.title !== title);
-//     }
-
-//     getBookByTitle(title: string): Book | undefined {
-//         return this.books.find(book => book.title === title);
-//     }
-
-//     getTotalNumberOfBooks(): number {
-//         return this.books.length;
-//     }
-
-//     getBooksByAuthor(author: string): Book[] {
-//         return this.books.filter(book => book.author === author);
-//     }
-
-//     getBooksByPublicationYear(publicationYear: number): Book[] {
-//         return this.books.filter(book => book.publicationYear === publicationYear);
-//     }
-// }
-
-// let lib: Library;
-// lib = new Library();
-
-// let book1: Book;
-// book1 = new Book('Clean Code', 'Edric Cao', 2023);
-
-// let book2: Book;
-// book2 = new Book('Design Pattern', 'Edric Cao', 2022);
-
-// lib.addBook(book1);
-// lib.addBook(book2);
-// console.log(lib.getBookByTitle('Clean Code')); // Output: Book { title: 'Clean Code', author: 'Edric Cao', publicationYear: 2023 }
-
+lib.addBook(book1);
+lib.addBook(book2);
+console.log(lib.getBookByTitle('Clean Code')); // Output: Book { title: 'Clean Code', author: 'Edric Cao', publicationYear: 2023 }
